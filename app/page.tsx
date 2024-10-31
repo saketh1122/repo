855bbb1210d0bb386ff9bba16404bfb612b479da
saketh <sakethm1122@gmail.com>
2024-10-31 15:13:42 +0530

    fuck off

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Page(): JSX.Element {
+  return (
+   <div className="text-2xl">
+    hi there
+   </div>
+  );
+}
